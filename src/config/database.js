90c58520e9932c996330d76abe131be60e043e11
Ryan Wagner <79729657+ryanzin1@users.noreply.github.com>
2024-03-29 00:44:07 -0300

    Add files via upload

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+ dialect: 'mysql',
+ host: 'localhost',
+ username: 'root',
+ password: '',
+ database: 'sqlnode',
+ define: {
+    timestamps: true,
+    underscored:true,
+
+ },
+
+};
---
+++ src/database/index.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+const dbConfig = require('../config/database.js');
+
+const User = require('../models/User')
+
+
+const connection = new Sequelize(dbConfig);
+
+User.init(connection)
+
+module.exports = connection
